c6f326adb5a41504bbe634f41c40efe750758697
thom.wingate <thom.wingate@gmail.com>
2019-08-08 01:13:44 -0400

    glitch somewhat fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,79 +15,8 @@
     //= require jquery_ujs
     //= require phrasing
     //= require turbolinks 
-    //= require_tree .
+    //= require_tree 
 
-
-// Haunt Pay Widget //
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552975","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552975"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552981","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552981"});
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552977","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552977"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552982","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552982"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552978","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552978"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552983","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552983"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552986","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552986"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552989","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552989"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552979","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552979"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552984","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552984"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552987","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552987"});
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552990","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552990"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552980","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552980"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552985","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552985"});
-
-
-
-
-window.passageWidgets.createCheckoutWidget({"modal":true,"triggerElementId":"widget-trigger-event-time-552988","iframeSrc":"https://app.hauntpay.com/events/5753/event_times/552988"});
 
 
 function initMap(){var e={lat:39.2967685,lng:-76.6028709},t=new google.maps.Map(document.getElementById("map"),{zoom:13,center:e,styles:[{elementType:"geometry",stylers:[{color:"#000000"}]},{elementType:"labels.text.stroke",stylers:[{color:"#242f3e"}]},{elementType:"labels.text.fill",stylers:[{color:"#7cc13c"}]},{featureType:"administrative.locality",elementType:"labels.text.fill",stylers:[{color:"#7cc13c"}]},{featureType:"poi",elementType:"labels.text.fill",stylers:[{color:"#7cc13c"}]},{featureType:"poi.park",elementType:"geometry",stylers:[{color:"#263c3f"}]},{featureType:"poi.park",elementType:"labels.text.fill",stylers:[{color:"#6b9a76"}]},{featureType:"road",elementType:"geometry",stylers:[{color:"#38414e"}]},{featureType:"road",elementType:"geometry.stroke",stylers:[{color:"#212a37"}]},{featureType:"road",elementType:"labels.text.fill",stylers:[{color:"#9ca5b3"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{color:"#746855"}]},{featureType:"road.highway",elementType:"geometry.stroke",stylers:[{color:"#1f2835"}]},{featureType:"road.highway",elementType:"labels.text.fill",stylers:[{color:"#e8f600"}]},{featureType:"transit",elementType:"geometry",stylers:[{color:"#2f3948"}]},{featureType:"transit.station",elementType:"labels.text.fill",stylers:[{color:"#e8f600"}]},{featureType:"water",elementType:"geometry",stylers:[{color:"#000"}]},{featureType:"water",elementType:"labels.text.fill",stylers:[{color:"#813cc1"}]},{featureType:"water",elementType:"labels.text.stroke",stylers:[{color:"#813cc1"}]}]}),l={src:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",scaledSize:new google.maps.Size(50,50),anchor:new google.maps.Point(25,25)},o=new google.maps.Marker({position:e,map:t,title:"The Nevermore Haunt",icon:l});google.maps.event.addListener(o,"click",function(){window.open("https://www.google.com/maps/place/The+Nevermore+Haunt/@39.2968134,-76.6082782,15z/data=!4m5!3m4!1s0x0:0x423340fda3926ce3!8m2!3d39.296913!4d-76.6028709")})}$(window).scroll(function(){$(document).scrollTop()>200?($("nav").addClass("shrink"),$("#nav-name").css("display","none")):($("nav").removeClass("shrink"),$("#nav-name").css("display","inline"))});
